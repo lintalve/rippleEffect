426b04fb4d674eebc39095b8919198860d58c602
lintalve <lintalve@icloud.com>
2024-02-22 19:42:19 +0100

    initial commit

---
+++ rippleEffect/rippleEffect.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rippleEffect
+//
+//  Created by Volodymyr on 22.02.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
